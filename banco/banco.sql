8acf13bafa7ac806330d7fd62c11ac64aebe64a0
marcioa <marcioalemao1999@yahoo.com>
2019-10-08 17:56:43 -0300

    criação do bando de dados e cadastro dos cursos e alunos

--- banco/banco.sql
+++ banco/banco.sql
@@ -11,18 +11,17 @@ default collate utf8_general_ci;
     cidade varchar(40) not null,
     endereco varchar(50) not null,
     senha char(8) not null
-    
- )default charset utf8;
+ )default charset = utf8;
  
  create table if not exists cursos(
 	id_curso int auto_increment primary key,
-    nomeCursos varchar(40) not null
- )default charset utf8;
- 
- alter table alunos modify id_curso int  unique not null;
+    nomeCursos varchar(40) unique not null
+ )default charset = utf8;
+  show tables;
+  
+ alter table alunos add column id_curso int  unique not null;
  alter table alunos add foreign key(id_curso) references cursos(id_curso);
- 
- alter table cursos modify nomeCursos varchar(40) unique not null;
+ show tables;
  
  describe alunos;
  describe cursos;
